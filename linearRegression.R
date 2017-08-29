e14162bd182115000c4ba3079e619e314f8d7340
ThorbenJensen <jensen.thorben@gmail.com>
2017-08-29 23:09:23 +0200

    added some distribution analysis and beta distribution fitting for dependent variable (number of bikes)

--- linearRegression.R
+++ linearRegression.R
@@ -2,11 +2,26 @@
 
 library(dplyr)
 library(ggplot2)
+library(fitdistrplus)
+library(MASS)
 
 # load data
 bikes <- read.csv("bikesNeutor1516.csv")
 
-## check features for normal distribution
+## check values for their distribution
+# number of bicycles
+descdist(bikes$noOfBikes, discrete = FALSE)
+# scale data
+bikes$noOfBikes_norm <- bikes$noOfBikes / (max(bikes$noOfBikes) + 10) + 0.001
+x <- dbeta(bikes$noOfBikes_norm, 0.927726829, 3.610553699)
+ggplot(data = bikes, aes(x)) + 
+  geom_histogram()
+fit.beta <- fitdistr(bikes$noOfBikes_norm, "beta", 
+                    list(shape1 = 2, shape2 = 5))
+ggplot(data = bikes, aes(bikes$noOfBikes_norm)) + 
+  geom_histogram()
+# TODO: clean transformation to normal distribution
+
 # temperature: approx. normally distributed
 ggplot(data = bikes, aes(bikes$temp)) + 
   geom_histogram()
