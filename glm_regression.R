256e7df41861d6663d76e1ffe6a44888dab1578e
ThorbenJensen <jensen.thorben@gmail.com>
2017-09-05 21:28:42 +0200

    gathered all GLM models in glm_regression.R

--- glm_regression.R
+++ glm_regression.R
@@ -35,3 +35,21 @@ summary(fit1)
 ## fit regression model ####
 fit <- lm(noOfBikes ~ temp + wind_log, data = bikes_filtered)
 summary(fit)
+
+## fit poisson regression (no transformation)
+fit1 <- glm(noOfBikes ~ temp + wind, 
+            data = bikes_filtered, 
+            family = poisson())
+summary(fit1)
+
+## fit negative binomial model (due to potentially high level of dispersion)
+fit2 <- glm.nb(noOfBikes ~ temp + wind, 
+               data = bikes_filtered)
+summary(fit2)
+
+## fit poisson regression (wind log-transformed)
+fit <- glm(noOfBikes ~ temp + wind_log, 
+           data = bikes_filtered,
+           family = poisson())
+summary(fit)
+
