6813ad5c69e842786e197053c545494960586fc0
ThorbenJensen <jensen.thorben@gmail.com>
2017-09-19 20:47:55 +0200

    moved files to directories

---
+++ src/BayesianModels.R
@@ -0,0 +1,154 @@
+# set working directory to proper directory
+# setwd("path/to/here")
+
+install.packages("brms")
+
+# library(fitdistrplus) for finding proper distributions --> TODO move to a better place
+library(brms)
+
+# TODO make a nice plot function to investigate the important properties of a model
+# plotModel <- function(model){
+#   pp_check(model)
+#   plot(marginal_effects(model), points = T, point_args = list(alpha = 0.05))#, jitter_width = 0.25)
+#   plot(model)
+# }
+
+# load data
+bikes = read.csv("bikesNeutor1516.csv")
+
+# ordered factors don't survive .csv storing, so, re-order weekdays:
+bikes$weekday = factor(bikes$weekday, 
+                       ordered = TRUE,
+                       levels = c("Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"))
+
+# look at the data -> see plotData.R
+# try to find a proper fitting distribution
+# x = bikes$noOfBikes
+# x = x[!is.na(x)]
+# descdist(x, discrete = FALSE)
+
+## Bayesian regression models
+
+# I have the feeling that exgaussian fits better -- but this should be checked ...
+# hour seems to be not working correct -- gives a weird regression line ...
+
+# TODO: do proper scaling of predictors
+# bikes$hourC = scale(bikes$hour, center = TRUE, scale = FALSE)
+
+## TODO have a look at update.brmsfit -> this might be quicker than re-computing all the time!
+
+#### exgaussian ####
+
+regressionModel_exgaussian_hours = 
+	brm(noOfBikes ~ poly(hour,3),
+			cores = 4,
+			family = exgaussian,
+			data = bikes)
+
+regressionModel_exgaussian_temp = 
+	brm(noOfBikes ~ poly(temp, 3),
+			cores = 4,
+			family = exgaussian,
+			data = bikes[!(is.na(bikes$temp)), ]) # TODO fix this before fitting the model ...
+
+regressionModel_exgaussian_wday = 
+	brm(noOfBikes ~ weekday,
+			cores = 4,
+			family = exgaussian,
+			data = bikes)
+
+regressionModel_exgaussian_wind = 
+	brm(noOfBikes ~ wind, # poly is not needed -> the model converges to a straight line
+			cores = 4,
+			family = exgaussian,
+			data = bikes[!is.na(bikes$wind), ])  # TODO fix this before fitting the model ...
+
+regressionModel_exgaussian_weather = 
+	brm(noOfBikes ~ weather,
+			cores = 4,
+			family = exgaussian,
+			data = bikes)
+
+regressionModel_exgaussian_month = 
+	brm(noOfBikes ~ month,
+			cores = 4,
+			family = exgaussian,
+			data = bikes)
+
+regressionModel_exgaussian_all = 
+	brm(noOfBikes ~ hours * temp * wday * wind * weather * month,
+			cores = 4,
+			family = exgaussian,
+			data = bikes)
+
+save(regressionModel_exgaussian_hours, 
+     regressionModel_exgaussian_temp, 
+     regressionModel_exgaussian_wday, 
+     regressionModel_exgaussian_wind, 
+     regressionModel_exgaussian_weather,
+     regressionModel_exgaussian_month,
+     regressionModel_exgaussian_all,
+     file = "exgaussian_models.RData")
+
+#### negbinomial ####
+
+regressionModel_negbinom_hours = 
+	brm(noOfBikes ~ poly(hour,3),
+			cores = 4,
+			family = negbinom,
+			data = bikes)
+
+regressionModel_negbinom_temp = 
+	brm(noOfBikes ~ poly(temp, 3),
+			cores = 4,
+			family = negbinom,
+			data = bikes[!(is.na(bikes$temp)), ]) # TODO fix this before fitting the model ...
+
+regressionModel_negbinom_wday = 
+	brm(noOfBikes ~ weekday,
+			cores = 4,
+			family = negbinom,
+			data = bikes)
+
+regressionModel_negbinom_wind = 
+	brm(noOfBikes ~ wind, # poly is not needed -> the model converges to a straight line
+			cores = 4,
+			family = negbinom,
+			data = bikes[!is.na(bikes$wind), ])  # TODO fix this before fitting the model ...
+
+regressionModel_negbinom_weather = 
+	brm(noOfBikes ~ weather,
+			cores = 4,
+			family = negbinom,
+			data = bikes)
+
+regressionModel_negbinom_month = 
+	brm(noOfBikes ~ month,
+			cores = 4,
+			family = negbinom,
+			data = bikes)
+
+regressionModel_negbinom_all = 
+	brm(noOfBikes ~ hours * temp * wday * wind * weather * month,
+			cores = 4,
+			family = negbinom,
+			data = bikes)
+
+save(regressionModel_negbinom_hours, 
+     regressionModel_negbinom_temp, 
+     regressionModel_negbinom_wday, 
+     regressionModel_negbinom_wind, 
+     regressionModel_negbinom_weather,
+     regressionModel_negbinom_month,
+     regressionModel_negbinom_all,
+     file = "negbinom_models.RData")
+
+# TODO
+# lognormal
+#regressionModel_lognormal = 
+#	brm(noOfBikes ~ hourC,
+#			cores = 4,
+#			family = lognormal,
+#			data = bikes[bikes$year == 2015 & bikes$month == 5, ])
+
+# poisson is totally wrong ...?
