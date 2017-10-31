222c9dfee5c33493ade80911c4be3a8d8aa7b352
Thorben Jensen <jensen.thorben@gmail.com>
2017-10-31 23:40:35 +0100

    extending frontend of shiny app without prediction part in the backend.

--- src/shinyapp/ui.R
+++ src/shinyapp/ui.R
@@ -1,23 +1,35 @@
 library(shiny)
 
 # Define UI for miles per gallon application
-shinyUI(pageWithSidebar(
+shinyUI(fluidPage(theme = "bootstrap.css",
   
   # Application title
   headerPanel("Cars on 'Wolbecker Str.'"),
   
   sidebarPanel(
-    selectInput("scale", "Time scale:",
-                list("Year" = "year",
-                     "Day" = "day")),
+
+    helpText("Show historical time series"),
+
+    radioButtons("scale", "Time scale:",
+                 c("Year" = "year",
+                   "Day" = "day")),
+
+    checkboxInput("grouping", "Group by direction"),
+
+    helpText("Predict new time lines"),
     
-    checkboxInput("grouping", "Group by direction")
+    # TODO: remove blue bar from slider
+    sliderInput("month", "Month", min = 1, max = 12, value = 6),
+    sliderInput("weekday", "Day of week", min = 1, max = 7, value = 3),
+    selectInput("weather", "Weather", c("Rain" = "rain", "Snow" = "snow")),
+    actionButton("button", "Predict traffic!")
   ),
-  
+
   mainPanel(
-    
+
     h3(textOutput("caption")),
-    
+
     plotOutput("plot")
+    # TODO: plot both cyclists and cars (in one plot each)
   )
 ))
