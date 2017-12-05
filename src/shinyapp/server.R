abb20085092416b402904e922da0973442769538
Thomas Kluth <t.kluth@posteo.de>
2017-12-05 22:12:37 +0100

    merge shiny app

--- src/shinyapp/server.R
+++ src/shinyapp/server.R
@@ -1,9 +1,13 @@
-library(shiny)
-library(datasets)
-library(RSQLite)
-library(sqldf)
-library(dplyr)
-library(ggplot2)
+# This program is free software.
+# You should have received a copy of the GNU General Public License
+# along with this program (file COPYING). If not, see <http://www.gnu.org/licenses/>.
+
+# SERVER LOGIC FOR THE SHINY APP
+
+# load libraries ####
+# use 00_install_R_packages.R for installing missing packages
+lapply(c("shiny", "datasets", "RSQLite", "dplyr", "sqldf", "ggplot2"), 
+       require, character.only = TRUE)
 
 wolbecker <- 
   sqldf("SELECT 
@@ -12,46 +16,9 @@ wolbecker <-
            WHEN 'MQ_09040_FV3_G (MQ1034)' THEN 'entering_city'
            WHEN 'MQ_09040_FV1_G (MQ1033)' THEN 'leaving_city'
            END 'direction'
-         FROM car_data
+         FROM kfz_data
          WHERE location LIKE '%09040%'", 
-        dbname = "../../data/database/kfz_data.sqlite") 
-
-p <- list()
-
-p[["year"]][["grouped"]] <-
-  wolbecker %>%
-  group_by(direction, date) %>%
-  summarise(count_day = sum(count)) %>%
-  ggplot(data = ., aes(x = date, y = count_day)) +
-  geom_line(aes(group = direction, color = direction)) +
-  theme_minimal(base_size = 18)
-
-p[["year"]][["ungrouped"]] <-
-wolbecker %>%
-  group_by(date) %>%
-  summarise(count_day = sum(count)) %>%
-  ggplot(data = ., aes(x = date, y = count_day)) +
-  geom_line(group = 1) +
-  theme_minimal(base_size = 18)
-
-p[["day"]][["grouped"]] <-
-  wolbecker %>%
-  ggplot(data = ., aes(x = hour, y = count)) +
-  geom_line(aes(group = interaction(date, direction), color = direction),
-            alpha = .2) +
-  theme_minimal() +
-  theme_minimal(base_size = 18)
-
-p[["day"]][["ungrouped"]] <-
-wolbecker %>%
-  group_by(date, hour) %>%
-  summarise(count_sum = sum(count)) %>%
-  ggplot(data = ., aes(x = hour, y = count_sum)) +
-  geom_line(aes(group = date),
-            alpha = .2) +
-  theme_minimal() +
-  theme_minimal(base_size = 18)
-
+        dbname = "../../data/processed/kfz_data.sqlite") 
 
 # Define server logic required to plot
 shinyServer(function(input, output) {
@@ -61,8 +28,39 @@ shinyServer(function(input, output) {
     paste("Time scale: ", input$scale)
   })
   
-  # Generate a plot of the requested scale
-  output$plot <- renderPlot({
-    p[[input$scale]][[ifelse(input$grouping, "grouped", "ungrouped")]]
+  filteredHourData <- reactive({
+    wolbecker %>% 
+      filter(
+        hour > input$hour_range[1] &
+          hour < input$hour_range[2],
+        date > as.POSIXct(input$date_range[1]) & 
+          date < as.POSIXct(input$date_range[2])
+      ) %>% 
+      group_by(date, direction, hour) %>%
+      summarise(count_sum = sum(count))
+    })
+    
+  filteredYearData <- reactive({
+    wolbecker %>%
+      filter(
+        hour > input$hour_range[1] &
+          hour < input$hour_range[2],
+        date > as.POSIXct(input$date_range[1]) &
+          date < as.POSIXct(input$date_range[2])
+      ) %>% 
+      group_by(date, direction) %>%
+      summarise(count_day = sum(count))
+  })
+
+  output$plotYear <- renderPlot({
+    ggplot(data = filteredYearData(), aes(x = date, y = count_day)) +
+    geom_line(aes(group = direction, color = direction)) +
+    theme_minimal(base_size = 18)
+  })
+  
+  output$plotDay <- renderPlot({
+    ggplot(data = filteredHourData(), aes(x = hour, y = count_sum)) +
+    geom_line(aes(group = interaction(date, direction), color = direction), alpha = .2) +
+    theme_minimal(base_size = 18)
   })
 })
