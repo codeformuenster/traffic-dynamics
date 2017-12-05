d6e8c27e20a8bbcb8870919fe21c2a075c9fd83c
Thorben Jensen <jensen.thorben@gmail.com>
2017-12-05 22:34:12 +0100

    adding 'biked to database' script.

---
+++ src/02_bikes_to_db.R
@@ -0,0 +1,21 @@
+# MOVE BIKES DATA TO DATABASE
+
+library(dplyr)
+
+file = "data/raw/Fahrradzaehlstellen-Stundenwerte.csv"
+
+df <- 
+  read.csv(file, sep = ";") %>%
+  # renaming columns
+  rename(hour = X) %>%
+  rename(date = Datum) %>%
+  # wide to long format
+  gather(location, count, -date, -hour)
+
+# write 'df' to SQLite database
+dir.create("data/database", showWarnings = F)
+con <- dbConnect(SQLite(), dbname = "data/database/traffic_data.sqlite")
+if (dbExistsTable(con, "bikes")) { dbRemoveTable(con, "bikes") }
+dbWriteTable(con, "bikes", df, row.names = F)
+dbDisconnect(con)
+
