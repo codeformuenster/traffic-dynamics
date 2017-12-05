e2688c91e946a18688875e7c7cea72ffacf796d7
Thomas Kluth <t.kluth@posteo.de>
2017-12-05 22:19:20 +0100

    renaming files

---
+++ src/06_h2o_models.R
@@ -0,0 +1,10 @@
+# This program is free software.
+# You should have received a copy of the GNU General Public License
+# along with this program (file COPYING). If not, see <http://www.gnu.org/licenses/>.
+
+# MACHINE LEARNING via H2O
+
+# load libraries ####
+# use 00_install_R_packages.R for installing missing packages
+lapply(c(""), 
+       require, character.only = TRUE)
