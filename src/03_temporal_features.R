fc3eca4f9e7ca5de349e042510deae82902f6f47
Thorben Jensen <jensen.thorben@gmail.com>
2017-12-09 22:51:43 +0100

    adapted engineering of temporal features to database

--- src/03_temporal_features.R
+++ src/03_temporal_features.R
@@ -2,28 +2,35 @@
 # You should have received a copy of the GNU General Public License
 # along with this program (file COPYING). If not, see <http://www.gnu.org/licenses/>.
 
-# ENGINEER TEMPORAL FEATURES AND STORE IN DATABASE
-
 # load libraries ####
 # if the following fails, you might want to use 00_install_R_packages.R
 # to install missing packages
 lapply(c("chron", "dplyr", "lubridate"), require, character.only = TRUE)
 
-# LOAD DATA ----
-bikes <- read.csv(file = "data/processed/bikes1516.csv")
+# LOAD DATES AND HOURS ----
+con <- dbConnect(SQLite(), dbname = "data/database/traffic_data.sqlite")
+temporal_features <-
+  dbGetQuery(conn = con, 
+             "SELECT DISTINCT * FROM (
+               SELECT date, hour FROM cars
+               UNION ALL
+               SELECT date_iso as date, hour_int as hour FROM bikes_processed
+             ) ORDER BY date ASC, hour ASC")
+dbDisconnect(con)
+
 
 # FEATURE ENGINEERING ----
-df <-
-  bikes %>%
-  select(date, hour, weather, wind, temp) %>%
+temporal_features <-
+  temporal_features %>%
   mutate(year = as.integer(year(date))) %>%
   mutate(month = as.integer(month(date))) %>%
-  mutate(weekday = wday(date, label = T, abbr = F)) %>%
-  mutate(weekend = is.weekend(date)) %>%
-  mutate(wind_log = log(wind)) %>%
-  mutate(rain = (weather == 'Regen'))
+  mutate(weekday = wday(date, label = T, abbr = T)) %>%
+  mutate(weekend = is.weekend(date))
+
+# TODO: add weather from Dark Sky API
 
 # SAVE TO DATABASE ----
-con <- dbConnect(SQLite(), dbname = "data/database/kfz_data.sqlite")
-dbWriteTable(conn = con, name = "temporal_features", value = df, overwrite = T)
+con <- dbConnect(SQLite(), dbname = "data/database/traffic_data.sqlite")
+dbWriteTable(conn = con, name = "temporal_features", value = temporal_features,
+             overwrite = T)
 dbDisconnect(con)
