9892be9b35d3e290f946acafbe2602b0544d046b
Thomas Kluth <t.kluth@posteo.de>
2018-01-09 21:52:41 +0100

    add another missing package to install list

--- src/00_install_R_packages.R
+++ src/00_install_R_packages.R
@@ -3,7 +3,7 @@
 # along with this program (file COPYING). If not, see <http://www.gnu.org/licenses/>.
 
 packages <- c("envDocument", "dplyr", "assertthat", "lubridate", "tidyr", "DBI",
-              "RSQLite", "ggplot2", "chron", "darksky")
+              "RSQLite", "ggplot2", "chron", "darksky", "RCurl")
 
 installed_packages <- installed.packages()[,"Package"]
 
