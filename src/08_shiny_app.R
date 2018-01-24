c38733251c92b66868346347a736cd48394c368b
Thomas Kluth <t.kluth@posteo.de>
2018-01-24 08:34:33 +0100

    remove shiny app from this backend-repo

--- src/08_shiny_app.R
+++
@@ -1,9 +0,0 @@
-# This program is free software.
-# You should have received a copy of the GNU General Public License
-# along with this program (file COPYING). If not, see <http://www.gnu.org/licenses/>.
-
-# RUN SHINY APP FOR INTERACTIVE DATA VISUALIZATION
-
-library(shiny)
-
-runApp("src/shinyapp")
